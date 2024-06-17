e34fb79d85137e8e533caa483425bc96db102d2b
Cuyox3 <artuciiz@gmail.com>
2024-06-16 20:51:54 -0600

    Avance seccion

--- sistema de ventas/test.js
+++ sistema de ventas/test.js
@@ -37,4 +37,51 @@ console.log(newProducto.toString());
 console.log(newProducto2.toString());
 
 class Orden{
+
+    static contadorOrdenes = 0;
     
+    static get MAX_PRODUCTOS(){
+        return 5;
+    }
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+    agregarProducto(producto){
+        if(this._productos.length < Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+            //this._productos[this._contadorProductosAgregados++] = producto;
+        }else{
+            console.log('No se pueden agregar mas productos');
+        }
+    }
+
+    calcularTotal(){
+        let totalVenta = 0;
+        for(let producto of this._productos){
+            totalVenta += producto.precio;
+        }
+        return totalVenta;
+    }
+
+    mostrartorden(){
+        let productosOrden = '';
+        for(let producto of this._productos){
+            productosOrden += producto.toString() + ' ';
+        }
+        console.log(`Orden: ${this._idOrden}, Total: $ ${this.calcularTotal()}, Productos: ${productosOrden}`);
+    }
+}
+
+let ornden1 = new Orden();
+let ornden2 = new Orden();
+ornden1.agregarProducto(newProducto);
+ornden2.agregarProducto(newProducto2);
+
+ornden1.mostrartorden();
+ornden2.mostrartorden();
