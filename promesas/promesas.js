968733f6b6b73aa01b6936dcddbd8268bbd1eb30
Cuyox3 <artuciiz@gmail.com>
2024-06-17 16:59:57 -0600

    Promesas

---
+++ promesas/promesas.js
@@ -0,0 +1,10 @@
+let mipromesa   = new Promise((resolve, reject) => {
+    let expresion = true;
+    if(expresion) {
+        resolve('¡Éxito!');
+    } else {
+        reject('¡Fallo!');
+    }
+});
+
+mi 
