ec5e0ce6b4c56bd0f7c228877643784db159d095
Cuyox3 <artuciiz@gmail.com>
2024-06-17 18:11:51 -0600

    Promesas

--- promesas/promesas.js
+++ promesas/promesas.js
@@ -1,5 +1,5 @@
-let mipromesa   = new Promise((resolve, reject) => {
-    let expresion = true;
+let mipromesa  = new Promise((resolve, reject) => {
+    let expresion = 0;
     if(expresion) {
         resolve('¡Éxito!');
     } else {
@@ -7,4 +7,20 @@ let mipromesa   = new Promise((resolve, reject) => {
     }
 });
 
-mi 
+mipromesa.then( valor => console.log(valor), error => console.log(error));
+mipromesa
+.then( valor => console.log(valor))
+.catch( error => console.log(error));
+
+// Promesas 
+
+let promesa = new Promise( (resolve) => {
+    setTimeout( () => resolve (
+        console.log("Promesa terminada"), 
+        resolve(), 
+        1500));
+});
+
+promesa.then( valor => console.log(valor));
+
+// async indica que una funcion regresa una promesa 
